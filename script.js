8c070712161a2c879a931ef0f33bd8b2bc8df73e
Jeffrey Chery <jeffchery009@gmail.com>
2024-02-21 01:32:06 -0500

    Functions that loads profiles during onClick Event

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function toggleMenuIcon() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function loadProfilePage() {
+  location.href = "https://www.linkedin.com/in/jeffreychery/";
+}
+
+function loadGithubPage() {
+  location.href = "https://github.com/jeffreychery009";
+}
